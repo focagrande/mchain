c3af209d87f71e7382ca92c30a6f6bfd7b4ff641
vagrant <vagrant@precise32.(none)>
2013-04-13 16:07:49 +0000

    Client modified

--- mchain_client_auth.py
+++ mchain_client_auth.py
@@ -1,12 +1,23 @@
 # -*- coding: utf-8 -*-
 
 import urllib2
+import json
+import sys
 
-jsonrpc_req = """
-{"jsonrpc": "2.0", "params": ["Poszla Dyna srac na tory", 1], "id": "0ccl8o0s", "method": "generate"}
-"""
+corpus_fname = './corpus/corpus_5.txt'
 
-req = urllib2.Request(url='http://localhost:5000', data=jsonrpc_req)
+with open(corpus_fname, 'r') as f:
+    corpus_txt = f.read()
+
+jsonrpc_req = {}
+jsonrpc_req['jsonrpc'] = "2.0"
+jsonrpc_req['params'] = [corpus_txt, 1]
+jsonrpc_req['id'] = '0ccl8o0s'
+jsonrpc_req['method'] = 'generate'
+
+jsonrpc_req_txt = json.dumps(jsonrpc_req)
+
+req = urllib2.Request(url='http://localhost:5000', data=jsonrpc_req_txt)
 req.add_header('Authorization', 'Basic enVtaWNybTp0WTVZSmY5MWFpUFJYRzA=')
 
 f = urllib2.urlopen(req)
