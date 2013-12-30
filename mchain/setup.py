88e7de9e2941997ba0b6718b8de9118fb639a393
Jacek  Spiewak <jacek.spiewak@gmail.com>
2013-12-30 12:18:55 +0000

    packaging mchain

---
+++ mchain/setup.py
@@ -0,0 +1,19 @@
+import os
+from setuptools import setup
+
+setup(
+    name = "mchain",
+    version = "0.0.1",
+    author = "Jacek Śpiewak",
+    author_email = "jacek.spiewak@gmail.com",
+    description = ("Human readable text generator based on Markov chains"),
+    license = "BSD",
+    keywords = "markov chain text generator",
+    url = "http://focagrande.github.io",
+    packages=['mchain'],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
